dd804635adff0f2a51d8e51e1b7c56d72defb1e7
Alex de Oliveira <alex.oliveira.web@gmail.com>
2016-03-03 20:41:51 -0300

    Testando unitariamente Action

--- src/Category/Controllers/AdminCategoriesController.php
+++ src/Category/Controllers/AdminCategoriesController.php
@@ -4,27 +4,32 @@ namespace TrezeVel\Category\Controllers;
 use TrezeVel\Category\Models\Category;
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Routing\ResponseFactory;
 
 /**
 * Controller de categorias
 */
 class AdminCategoriesController extends Controller
 {
-	private $category;
-	public function __construct(Category $category)
+    private $category;
+	private $response;
+
+	public function __construct(ResponseFactory $response, Category $category)
 	{
-		$this->category = $category;
+        $this->category = $category;
+		$this->response = $response;
 	}
+
     public function index()
     {
         $categories = $this->category->all();
-        return view('trezevelCategory::index')->with(compact('categories'));
+        return $this->response->view('trezevelCategory::index')->with(compact('categories'));
     }
 
     public function create()
     {
     	$categories = $this->category->all();
-    	return view('trezevelCategory::create')->with(compact('categories'));
+    	return $this->response->view('trezevelCategory::create')->with(compact('categories'));
     }
 
     public function store(Request $request)
--- tests/Category/Controllers/AdminCategoriesControllerTest.php
+++ tests/Category/Controllers/AdminCategoriesControllerTest.php
@@ -7,6 +7,7 @@ use TrezeVel\Category\Controllers\Controller;
 use TrezeVel\Category\Models\Category;
 use TrezeVel\Category\Controllers\AdminCategoriesController;
 use Mockery as m;
+use Illuminate\Contracts\Routing\ResponseFactory;
 
 /**
 * Classe de teste de categoria
@@ -17,9 +18,32 @@ class AdminCategoriesControllerTest extends AbstractTestCase
 	public function testShouldExtendFromController()
 	{
 		$category = m::mock(Category::class);
+		$responseFactory = m::mock(ResponseFactory::class);
 
-		$controller = new AdminCategoriesController($category);
+		$controller = new AdminCategoriesController($responseFactory, $category);
 
 		$this->assertInstanceOf(Controller::class, $controller);
+	}
+
+
+	public function textControllerShouldRunIndexMethodAndReturnCorrectArguments()
+	{
+		
+		$category = m::mock(Category::class);
+		$responseFactory = m::mock(ResponseFactory::class);
+
+		$html = m::mock();
+
+		$controller = new AdminCategoriesController($responseFactory, $category);
+
+		$categoriesResult = ['cat1', 'cat2'];
+		$category->shouldReceive('all')->andReturn($categoriesResult);
+
+		$responseFactory->shouldReceive('view')
+			->with('trezevelCategory::index', ['categories' => $categoriesResult])
+			->andReturn($html);
+
+		$this->assertEquals($controller->index(), $html);
+
 	}
 }
